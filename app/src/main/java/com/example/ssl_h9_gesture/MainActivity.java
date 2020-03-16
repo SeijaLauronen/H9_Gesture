20d943c9a732bf411c5fdad7698d6aaf6754ae73
SeijaLauronen <seija.lauronen@gmail.com>
2020-03-16 10:29:38 +0200

    Nyt on värit säädetty.

--- app/src/main/java/com/example/ssl_h9_gesture/MainActivity.java
+++ app/src/main/java/com/example/ssl_h9_gesture/MainActivity.java
@@ -28,15 +28,51 @@ public class MainActivity extends AppCompatActivity {
             float y2 = e2.getY() ;
             String tooooast = Float.toString(x)+ " " +Float.toString(y);
             tooooast += " " + Float.toString(x2)+ " " +Float.toString(y2);
-            Toast.makeText(MainActivity.this,tooooast, Toast.LENGTH_SHORT).show();
 
-            //maxarvo 255
-            int redValue = (int) velocityX;
-            int bluValue = (int) velocityY;
-            //leiaut.setBackgroundColor(Color.rgb(113,123,123));
+            tooooast = Float.toString(velocityX)+ " Y:" +Float.toString(velocityY);
 
-            leiaut.setBackgroundColor(Color.rgb(redValue,123,bluValue));
+            //Toast.makeText(MainActivity.this,tooooast, Toast.LENGTH_SHORT).show();
 
+            //värin maxarvo 255
+
+            //Nopeuksien maxarvot? 16000 jossain kai, mutta omassa jotain muuta
+            Integer maxVX =24000;//25000;
+            Integer maxVY =24000;//25000;
+            Integer dividerX =maxVX/255;
+            Integer dividerY =maxVY/255;
+
+
+            int redValue = (int) velocityX/dividerX; //140;
+            int bluValue = (int) velocityY/dividerY; //240;
+            //En nyt jaksa etisä mikä se oli se itseisarvo...
+            //miksi ylipäätään velocitystä tulee negatiivia arvoja?
+
+            if (redValue<0){
+                redValue = redValue *-1;
+            }
+            if (bluValue<0){
+                bluValue = bluValue *-1;
+            }
+
+
+        //Tässä valittaa, jos antaa negatiivisia arvoja, mutta toimii kuitenkin niilläkin
+            //leiaut.setBackgroundColor(Color.rgb(255,0,255));
+
+            leiaut.setBackgroundColor(Color.rgb(redValue,0,bluValue));
+            tooooast="";
+            tooooast+= "   RED " + Integer.toString(redValue);
+            if (redValue>255 || redValue < 0) {
+                tooooast+= " RED meni yli ";// + Integer.toString(redValue);
+                //Toast.makeText(MainActivity.this, tooooast, Toast.LENGTH_SHORT).show();
+            }
+
+            tooooast+= "   BLUE : " + Integer.toString(bluValue);
+            if (bluValue>255 || bluValue < 0) {
+                tooooast+= " BLUE meni yli ";//  + Integer.toString(bluValue);
+                //Toast.makeText(MainActivity.this, tooooast, Toast.LENGTH_SHORT).show();
+            }
+
+            Toast.makeText(MainActivity.this, tooooast, Toast.LENGTH_SHORT).show();
 
 
             return super.onFling(e1, e2, velocityX, velocityY);
